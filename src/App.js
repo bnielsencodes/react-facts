8cb9ea5dcb799981d023b28cde5d6606562eaae6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-11 00:09:32 -0800

    Add <Navbar /> to <App />

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
 export default function App() {
 
   return (
     <div className="container">
+      <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
     </div>
   );
 }
