825ec9f0614c2315d8ffdc16e8664a744d887d47
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-11 00:09:46 -0800

    Add <Main /> to <App />

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
 export default function App() {
 
   return (
     <div className="container">
       <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
